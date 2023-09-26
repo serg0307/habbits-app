9d2fd9c29a7dfae106036a6aa7978f071e399c07
serg0307 <p14@bk.ru>
2023-09-26 13:46:31 +0300

    text errors fixed

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'habbits'`, () => {
+  it(`should have as title 'habits'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('habbits');
+    expect(app.title).toEqual('habits');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('habbits app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('habits app is running!');
   });
 });
